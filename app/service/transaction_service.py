946e477910e161cb5d4a55b0077fa789ec383687
DonJosueSuarez <josuea.suarezs@gmail.com>
2026-02-12 22:51:54 -0500

    Se refactorizaron métodos create_many

---
+++ app/service/transaction_service.py
@@ -0,0 +1,15 @@
+from typing import List
+from app.repository.base_transaction_repository import ITransactionRepository
+from app.dto.transaction_dtos import TransactionCreate
+from app.model.transaction import TransactionORM
+class TransactionService:
+    def __init__(self, repository: ITransactionRepository):
+        self.repository = repository
+        
+def create_many_transactions(self, transactions: List[TransactionCreate]):
+    # El truco: **t.model_dump() expande el DTO directamente al modelo
+    new_transactions = [
+        TransactionORM(**t.model_dump()) 
+        for t in transactions
+    ]
+    return self.repository.create_many(new_transactions)
